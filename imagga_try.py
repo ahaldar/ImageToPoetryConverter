d8d9b0014eb8a1f55032fed141e414d74aaad9be
Siddharth <f2012519@goa.bits-pilani.ac.in>
2016-04-07 15:42:03 +0000

    Some more changes

--- imagga_try.py
+++ imagga_try.py
@@ -1,12 +1,12 @@
 import requests
-import dataset()
+import dataset
 
 api_file = open("api_details.txt", "r")
 
 api_key = api_file.readline().strip()
 api_secret = api_file.readline().strip()
 api_file.close()
-image_url = 'http://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
+#image_url = 'http://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
 
 
 def get_tags(image_url):
@@ -17,12 +17,14 @@ def get_tags(image_url):
 	for tag in text['results'][0]['tags']:
 		if tag['confidence'] > 70:
 			tag_list += (tag['tag'], tag['confidence'])
+			table.insert(url=image_url, tag=tag['tag'], confidence=tag['confidence'])
 	return tag_list
 
 def main():
-	image_url = raw_input().strip()
+	print "here"
+	image_url = raw_input("Please enter the link you want to get a poem from:").strip()
 	print get_tags(image_url)
 
-for tag in text['results'][0]['tags']:
-    if tag['confidence'] > 70:
-        table.insert(url=image_url, tag=tag['tag'], confidence=tag['confidence'])
+
+if __name__ == "__main__":
+	main()
