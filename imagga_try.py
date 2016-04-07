51ccf962566036a88f940a19a486caedc4ba175f
Siddharth <f2012519@goa.bits-pilani.ac.in>
2016-04-07 15:37:28 +0000

    refactoring

--- imagga_try.py
+++ imagga_try.py
@@ -7,11 +7,18 @@ api_secret = api_file.readline().strip()
 api_file.close()
 image_url = 'http://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
 
-response = requests.get('https://api.imagga.com/v1/tagging?url=%s' % image_url, auth=(api_key, api_secret))
 
-text =	response.json()
+def get_tags(image_url):
+	response = requests.get('https://api.imagga.com/v1/tagging?url=%s' % image_url, auth=(api_key, api_secret))
 
-for tag in text['results'][0]['tags']:
-	if tag['confidence'] > 70:
-		print tag['tag'] , ' ', tag['confidence']
+	text =	response.json()
+	tag_list = []
+	for tag in text['results'][0]['tags']:
+		if tag['confidence'] > 70:
+			tag_list += (tag['tag'], tag['confidence'])
+	return tag_list
+
+def main():
+	image_url = raw_input().strip()
+	print get_tags(image_url)
 
