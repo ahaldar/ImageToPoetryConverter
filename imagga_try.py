30a899834d88f1d151618af5c356a1460f3d715f
Siddharth <f2012519@goa.bits-pilani.ac.in>
2016-04-07 15:16:21 +0000

    extract tags from api

--- imagga_try.py
+++ imagga_try.py
@@ -1,11 +1,17 @@
 import requests
 
+api_file = open("api_details.txt", "r")
 
-api_key = ''
-api_secret = ''
+api_key = api_file.readline().strip()
+api_secret = api_file.readline().strip()
+api_file.close()
 image_url = 'http://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg'
 
-response = requests.get('https://api.imagga.com/v1/tagging?url=%s' % image_url,
-                auth=(api_key, api_secret))
+response = requests.get('https://api.imagga.com/v1/tagging?url=%s' % image_url, auth=(api_key, api_secret))
 
-print response.json()
+text =	response.json()
+
+for tag in text['results'][0]['tags']:
+	if tag['confidence'] > 70:
+		print tag['tag'] , ' ', tag['confidence']
+
