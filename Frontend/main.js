34d85e8f7717a504b343e25ec622e33ef2654e37
Ruchi Ookalkar <rookalkar@gmail.com>
2016-04-29 08:37:52 +0000

    Front end for Screen 1,2

---
+++ Frontend/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    
+    var myEl = document.getElementById('temp');
+    myEl.addEventListener('click', function() {
+        console.log('Hello world');
+    }, false);
+});
